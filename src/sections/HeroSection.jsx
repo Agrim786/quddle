b211781507742fee12090d3202768bf358a50d5e
Agrim786 <agrimmahajam786@gmail.com>
2025-11-29 00:36:20 +0530

    Image Changed

--- src/sections/HeroSection.jsx
+++ src/sections/HeroSection.jsx
@@ -118,8 +118,8 @@ const HeroSection = () => {
 
         {/* ---------- MOUSE SCROLL ---------- */}
         <div className="mouse-scroll absolute bottom-8 left-1/2 transform -translate-x-1/2 z-30">
-          <div className="w-6 h-10 border-2 border-black rounded-full flex justify-center">
-            <div className="w-1 h-3 bg-black rounded-full mt-2 animate-pulse" />
+          <div className="w-6 h-10 border-2 border-white rounded-full flex justify-center">
+            <div className="w-1 h-3 bg-white rounded-full mt-2 animate-pulse" />
           </div>
         </div>
 
